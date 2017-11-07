87841ffdee5a4c25e714ee55e83f6adf05825f6b
jericopulvera <jerico.pulvera01z@gmail.com>
2017-11-07 18:59:19 +0800

    v1 webpack tailwind setup

---
+++ build/vue-loader.conf.js
@@ -0,0 +1,25 @@
+'use strict'
+const utils = require('./utils')
+const config = require('../config')
+const isProduction = process.env.NODE_ENV === 'production'
+var tailwindcss = require('tailwindcss');
+
+module.exports = {
+  loaders: utils.cssLoaders({
+    sourceMap: isProduction
+      ? config.build.productionSourceMap
+      : config.dev.cssSourceMap,
+    extract: isProduction
+  }),
+  transformToRequire: {
+    video: 'src',
+    source: 'src',
+    img: 'src',
+    image: 'xlink:href'
+  },
+  postcss: [
+    require('tailwindcss')('./tailwind.js'),
+    require('autoprefixer')
+  ],
+  extractCSS: true
+}
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <main>
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './assets/sass/app.scss';
+</style>
+
